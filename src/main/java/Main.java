f447b818e54d687c1035e6690cdf950d242360e8
Paxton Edgar <paxtonedgar3@gmail.com>
2022-07-13 17:00:17 -0400

    init push

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        // your code here
+        String message1 = "Sample out put line 1";
+        String message2 = "Sample out put line 2";
+
+        Logger logger = Logger.getInstance();
+
+        logger.log(message1);
+        logger.log(message2);
     }
 }
